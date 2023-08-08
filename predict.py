ab062ed6c5147180a038ea94bae2495b013dcbd7
spencerkarofsky <105813301+spencerkarofsky@users.noreply.github.com>
2023-08-08 12:53:18 -0400

    Update predict.py

--- predict.py
+++ predict.py
@@ -4,21 +4,20 @@ Spencer Karofsky
 """
 from ultralytics import YOLO
 import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image
 import cv2
 
 model = YOLO("yolov8n.pt")  # load pretrained YOLOv8 model on COCO
 
 # Creating a VideoCapture object to read the video
-cap = cv2.VideoCapture('traffic-video.mp4')
+cap = cv2.VideoCapture('highway.mp4')
 
 # Loop until the end of the video
 while (cap.isOpened()):
 
     # Capture frame-by-frame
     ret, frame = cap.read()
-    frame = cv2.resize(frame,(1000,600))
+    frame = cv2.resize(frame,(1200,700))
+    frame = cv2.convertScaleAbs(frame,alpha=1.5,beta=-50)
     results = model(frame)  # return a list of Results objects
 
     # Process results list
@@ -38,16 +37,10 @@ while (cap.isOpened()):
         width = x2 - x1
         height = y2 - y1
         cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),2)
-        '''# plot bounding box
-        rect = plt.Rectangle((x1, y1), width, height, fill=False, edgecolor=(0, 1, 0), linewidth=2)
-        plt.gca().add_patch(rect)
+
         # use class dictionary to convert integer to correpsponding label and add to plot
         label = class_names.get(int(cls[i]))
-        plt.text(x1, y1 - 5, label, fontsize=12, color=(0, 1, 0))
-        '''
-    # display the image
-    #cv2.imshow(frame)
-    #plt.show()
+        cv2.putText(frame,label,(x1,y1-5),cv2.FONT_HERSHEY_DUPLEX,.75 ,(0,255,0),2)
 
 
     # Display the resulting frame
@@ -69,5 +62,6 @@ Sources:
 3) https://docs.ultralytics.com/reference/engine/results/#ultralytics.engine.results.Boxes
 4) https://www.geeksforgeeks.org/python-process-images-of-a-video-using-opencv/
 5) https://docs.opencv.org/3.4/dc/da5/tutorial_py_drawing_functions.html
-
+6) https://www.tutorialspoint.com/how-to-change-the-contrast-and-brightness-of-an-image-using-opencv-in-python
+7) 
 '''
