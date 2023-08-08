7d9011d8568a61c84a7f6010b2ce849a3f4aee14
spencerkarofsky <105813301+spencerkarofsky@users.noreply.github.com>
2023-08-08 11:53:29 -0400

    Update predict.py

--- predict.py
+++ predict.py
@@ -6,33 +6,39 @@ from ultralytics import YOLO
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+import cv2
 
 model = YOLO("yolov8n.pt")  # load pretrained YOLOv8 model on COCO
 
-#img = 'https://ultralytics.com/images/bus.jpg'
 img = './Oxford-Street.jpg'
 img = np.asarray(Image.open(img))
 
-
 results = model(img)  # return a list of Results objects
+
 # Process results list
 for result in results:
     boxes = result.boxes.xyxy
     cls = result.boxes.cls
     probs = result.probs  # Probs object for classification outputs
 
-class_names = result.names
+class_names = result.names # dictionary of COCO classes
 
 for i in range(len(boxes)):
-    width = boxes[i][2]-boxes[i][0]
-    height = boxes[i][3]-boxes[i][1]
+    # calculate bounding boxes
     x1 = boxes[i][0]
     y1 = boxes[i][1]
-    label = class_names.get(int(cls[i]))
+    x2 = boxes[i][2]
+    y2 = boxes[i][3]
+    width = x2-x1
+    height = y2-y1
+    # plot bounding box
     rect = plt.Rectangle((x1, y1), width, height, fill=False, edgecolor=(0,1,0), linewidth=2)
     plt.gca().add_patch(rect)
+    # use class dictionary to convert integer to correpsponding label and add to plot
+    label = class_names.get(int(cls[i]))
     plt.text(x1, y1-5, label, fontsize=12, color=(0,1,0))
 
+# display the image
 plt.imshow(img)
 plt.show()
 
