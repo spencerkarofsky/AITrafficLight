f56dbfcd5dd9df6984188bf7b4c05a0278b57b4d
spencerkarofsky <105813301+spencerkarofsky@users.noreply.github.com>
2023-08-08 09:28:02 -0400

    Create predict.py

---
+++ predict.py
@@ -0,0 +1,42 @@
+"""
+"""
+from ultralytics import YOLO
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+model = YOLO("yolov8n.pt")  # load pretrained YOLOv8 model on COCO
+
+#img = 'https://ultralytics.com/images/bus.jpg'
+img = './Oxford-Street.jpg'
+img = np.asarray(Image.open(img))
+
+
+results = model(img)  # return a list of Results objects
+# Process results list
+for result in results:
+    boxes = result.boxes.xyxy
+    cls = result.boxes.cls
+    probs = result.probs  # Probs object for classification outputs
+
+for i in range(len(boxes)):
+    width = boxes[i][2]-boxes[i][0]
+    height = boxes[i][3]-boxes[i][1]
+    x1 = boxes[i][0]
+    y1 = boxes[i][1]
+    rect = plt.Rectangle((x1, y1), width, height, fill=False, edgecolor=(0,1,0), linewidth=2)
+    plt.gca().add_patch(rect)
+    plt.text(x1 + width / 2, y1 + height / 2, int(cls[i]), fontsize=12, color=(0,1,0))
+
+plt.imshow(img)
+plt.gca().add_patch(rect)
+plt.show()
+
+
+'''
+Sources:
+1) https://github.com/ultralytics/ultralytics
+2) https://docs.ultralytics.com/usage/python/
+3) https://docs.ultralytics.com/reference/engine/results/#ultralytics.engine.results.Boxes
+4) 
+'''
