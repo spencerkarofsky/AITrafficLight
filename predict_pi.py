30339b20297727e0a67ec4539e8b8528489fdc42
spencerkarofsky <105813301+spencerkarofsky@users.noreply.github.com>
2023-09-14 08:53:24 -0400

    Update predict_pi.py

--- predict_pi.py
+++ predict_pi.py
@@ -11,9 +11,13 @@ import os
 import Detector
 import Motor
 import TrafficLight
+import random
+
+
+# Constants
+VEHICLE_TIME_CONSTANT = 2  # time, in seconds, per vehicle, of green light time
 
 # Functions
-
 '''
 take_pic()
 Inputs: none
@@ -40,6 +44,9 @@ def main():
         data_row = []
         # For the first column in data_row, 'Date/Time', add the date and time using the datetime module
         data_row.append(str(datetime.datetime.now()))
+
+        # Create traffic queue to store priority of street light
+        traffic_queue = []
 
         # The motor will rotate 90 degrees 4 times to capture the 4 different streets/views before turning back 360 degrees
         for turn in range(4):
@@ -68,20 +75,54 @@ def main():
         # Case 1: All sides have less than 2 cars; intersection will act as a 4-way stop.
         if data_row[1] <= 2 and data_row[2] <= 2 and data_row[3] <= 2 and data_row[3] <= 2:
             # turn all greens and yellows off; all reds  blink to signal 4-way stop
-            pass
+            traffic_light.set_flash_red('Street 1')
+            traffic_light.set_flash_red('Street 2')
         # Case 2: some sides have more than 2 cars, so the ratio between the 2 streets will be calculated using the calculate_traffic_ratio function.
         # That ratio will then be used to determine the timing of the lights.
         else:
             ratio = traffic_light.calculate_traffic_ratio(data_row)
             # Case A: Street 2 has more traffic than Street 1; Street 2 gets green light priority
             if ratio < 1:
-                # set street 1 lights (green[0] and green[3]) to green and street 2 lights to red.
-                pass
+                # Green light time = a constant * the average number of vehicles on a given street
+                green_light_time = traffic_light.get_green_light_time('Side 2')
+
+                # Set street 2 lights (green[1] and green[3]) to green and street 1 lights to red.
+
+
+                traffic_queue.append(['Side 2','Side 4'],)
             # Case B: Street 1 has more traffic than Street 2; Street 1 gets green light priority.
             elif ratio > 1:
-                pass
+                # Green light time = a constant * the average number of vehicles on a given street
+                green_light_time = traffic_light.get_green_light_time('Side 2')
+
+                # Set street 1 lights (green[0] and green[2]) to green and street 2 lights to red.
+
             else:
-                pass
+                # The streets have equal traffic amounts.
+
+                # If traffic_dataframe row is first row, randomly select street to get green light priority
+                current_row_index = traffic_light.traffic_dataframe.index.get_loc(traffic_light.traffic_dataframe.index[0])
+                if current_row_index == 0:
+                    random_street = random.randint(1, 2)
+                    if random_street == 1:
+                        # Street 1 gets green light priority
+                        green_light_time = traffic_light.get_green_light_time('Side 1')
+                        # Set green and red lights
+                    else:
+                        # Street 2 gets green light priority
+                        green_light_time = traffic_light.get_green_light_time('Side 2')
+                        # Set green and red lights
+                # Else, give green light priority to the last street to have red light.
+                else:
+                    if (traffic_light.traffic_dataframe.iloc[-2, 1] + traffic_light.traffic_dataframe.iloc[-2, 3]) > (traffic_light.traffic_dataframe.iloc[-2, 2] + traffic_light.traffic_dataframe.iloc[-2, 4]):
+                        pass
+                    else:
+                        street_green = traffic_light.get_last_green()
+                        traffic_light.set_green_light(street_green)
+                        if street_green == 'Street 1':
+                            traffic_light.set_red_light('Street 2')
+                        else:
+                            traffic_light.set_red_light('Street 1')
 
         # Rotate Counter-Clockwise back to starting position
         motor.rotate_count_clock(360)
