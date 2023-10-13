0301408ddbd2ccb8e8b995f15e867bbb84c3bcc0
spencerkarofsky <105813301+spencerkarofsky@users.noreply.github.com>
2023-10-13 18:52:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ Spencer R. Karofsky
 import cv2
 import datetime
 import os
-import Detector
-import Motor
-import TrafficLight
+from Detector import Detector
+from Motor import Motor
+from TrafficLight import TrafficLight
 
 # Global Function
 '''
@@ -21,6 +21,7 @@ def take_pic():  # take picture using fswebcam command line tool
 
 '''
 main()
+This program
 '''
 def main():
     # Create instance of Detector class
@@ -28,7 +29,7 @@ def main():
 
     # Create instance of Motor class to represent the stepper motor
     motor_channel = (29, 31, 33, 35)
-    motor = Motor(motor_channel)
+    motor = Motor(29,31,33,35)
 
     # Create instance of a TrafficLight class
     traffic_light = TrafficLight()
@@ -37,19 +38,19 @@ def main():
     while True:
         # Create new data row
         data_row = []
-        # For the first column in data_row, 'Date/Time', add the date and time using the datetime module
+        # For the first column in data_row, 'Date/Time', add the date and time using the dateti>
         data_row.append(str(datetime.datetime.now()))
 
         # Create traffic queue to store priority of street light
         traffic_queue = []
 
-        # The motor will rotate 90 degrees 4 times to capture the 4 different streets/views before turning back 360 degrees
+        # The motor will rotate 90 degrees 4 times to capture the 4 different streets/views bef>
         for turn in range(4):
             motor.rotate_clock(90)
             take_pic()
             frame = cv2.imread('img.jpg')
 
-            # Get the number of vehicles in the frame by calling the detect_objects Detector method
+            # Get the number of vehicles in the frame by calling the detect_objects Detector me>
             detections = detector.detect_objects(frame)
 
             # Get number of vehicles from detections list
@@ -58,7 +59,7 @@ def main():
                 if object in ['car', 'motorcycle','bus','truck']:
                     vehicle_count += 1
 
-            # Append traffic_count value to data_row list (to later be added to the traffic_dataframe)
+            # Append traffic_count value to data_row list (to later be added to the traffic_dat>
             data_row.append(vehicle_count)
 
         # Add the new data as a row to the DataFrame
@@ -71,6 +72,7 @@ def main():
         motor.rotate_count_clock(360)
 
 main()
+
 
 '''
 Sources:
